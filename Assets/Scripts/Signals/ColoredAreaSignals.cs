85f3e3023cebf59d5f9a4edb60a99834f2783e8f
Erdem <58220908+sirivlucifer@users.noreply.github.com>
2022-08-15 17:02:20 +0300

    Drone area bug fixed

---
+++ Assets/Scripts/Signals/ColoredAreaSignals.cs
@@ -0,0 +1,15 @@
+﻿
+using Extentions;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Signals
+{
+    public class ColoredAreaSignals : MonoSingleton<CollectableSignals>
+    { 
+        //PLAYERDA İŞ YAPACAK. BAŞKA SINYAL AÇILABILIR.
+        public UnityAction<GameObject> onEnterGroundCheck=delegate {  }; //player yavaslatma ıslemı yapılacak. YAPILMADI.
+        public UnityAction<GameObject> onExitGroundCheck=delegate {  }; //stackta killendi 
+        public UnityAction<GameObject> onTurrentGroundControll = delegate {  };
+    }
+}
